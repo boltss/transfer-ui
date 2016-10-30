e82ee0658b2eb16cc778139d1110320fe0953ea7
Roger Kerse <rogerkerse@gmail.com>
2016-10-30 20:45:36 +0200

    :scissors: Remove demo code

--- app/app.js
+++ app/app.js
@@ -2,21 +2,10 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends Component {
-
-	constructor(props) {
-		super(props);
-
-		this.onClick = this.onClick.bind(this);
-	}
-
-	onClick() {
-		a = 2/0;
-	}
 	render() {
 		return (
 			<div>
 				<h1>React works!</h1>
-				<button onClick={this.onClick}>Click</button>
 			</div>
 		)
 	}
