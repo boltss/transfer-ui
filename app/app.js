7b3c04d9b3641b2c10388d24e6dcc5636cc8a6bd
Roger Kerse <rogerkerse@gmail.com>
2016-10-30 21:14:55 +0200

    :new: Add support for ES2015+

--- app/app.js
+++ app/app.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 class App extends Component {
+	state = {
+		count: 0
+	}
+	increase = () => {
+		this.setState({
+			count: this.state.count + 1
+		});
+	}
 	render() {
 		return (
 			<div>
 				<h1>React works!</h1>
+				<button onClick={this.increase}>Suurenda</button>
 			</div>
 		)
 	}
