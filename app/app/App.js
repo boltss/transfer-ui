ee8933d77c8b1da8ce274c97633a87b05e984d98
Roger Kerse <rogerkerse@gmail.com>
2016-10-31 00:35:54 +0200

    :new: Add drop area for files

--- app/app/App.js
+++ app/app/App.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react'
+import Dropzone from 'react-dropzone'
 
 export default class App extends Component {
 	state = {
-		count: 0
+		draggingFile: false
 	}
-	increase = () => {
+	onDropFile = (e) => {
+		this.onStopDraggingFile();
+		console.log(e)
+	}
+	onDragFile = (e) => {
 		this.setState({
-			count: this.state.count + 1
-		})
+			draggingFile: true
+		});
+	}
+	onStopDraggingFile = (e) => {
+		this.setState({
+			draggingFile: false
+		});
 	}
 	render() {
+		const {
+			draggingFile
+		} = this.state;
+		const dropZoneTextActiveStyle = draggingFile ? styles.dropZoneTextActive: {}
 		return (
-			<div>
-				<h1>React works! {this.state.count}</h1>
-				<button onClick={this.increase}>Suurenda</button>
-			</div>
+			<Dropzone
+				onDrop={this.onDropFile}
+				disableClick={true}
+				style={styles.dropZone}
+				onDragEnter={this.onDragFile}
+				onDragOver={this.onDragFile}
+				onDragLeave={this.onStopDraggingFile}
+				onDragEnd={this.onStopDraggingFile}>
+				<div style={{...styles.dropZoneText, ...dropZoneTextActiveStyle}}>
+					Drop file you wish to transfer here
+				</div>
+			</Dropzone>
 		)
+	}
+}
+
+const styles = {
+	dropZone: {
+		width: '100%',
+		height: '100%',
+		padding: '50px',
+		boxSizing: 'border-box'
+	},
+	dropZoneText: {
+		height: '100%',
+		borderStyle: 'dashed',
+		borderColor: 'lightgray',
+		boxSizing: 'border-box',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	dropZoneTextActive: {
+		borderColor: 'black'
 	}
 }
